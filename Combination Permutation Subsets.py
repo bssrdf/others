1f6dc89100d254a231904d83b39e93322cadc359
avocadoccme <yiransh@gmail.com>
2015-01-27 18:27:28 -0800

    01-27-2015 fmc

---
+++ Combination Permutation Subsets.py
@@ -0,0 +1,72 @@
+class Solution:
+    # input are unique numbers
+    def permutation(self, nums):
+        if len(nums)==0: return [[]]
+        nums.sort()
+        res = []
+        for i in range(len(nums)):
+            remains = self.permutation(nums[:i]+nums[i+1:])
+            for remain in remains:
+                res.append(remain+[nums[i]])
+        return res
+
+    # input contain duplicates
+    def permutation2(self, nums):
+        if len(nums)==0: return [[]]
+        nums.sort()
+        res = []
+        for i in range(len(nums)):
+            if i==0 or nums[i]!=prev:
+                prev = nums[i]
+                remains = self.permutation2(nums[:i]+nums[i+1:])
+                for remain in remains: res.append(remain+[nums[i]])
+            else: continue
+        return res
+
+    # input are unique numbers
+    def subsets(self, nums):
+        if len(nums) == 0: return [[]]
+        nums.sort()
+        currents = [[]]
+        for num in nums:
+            next = []
+            for cur in currents:
+                next.append(cur[:])
+                next.append(cur+[num])
+            currents = next
+        return currents
+
+    # input contain duplicates
+    def subsets2(self, nums):
+        if len(nums) == 0: return [[]]
+        nums.sort()
+        currents = [[]]
+        for num in nums:
+            next = []
+            for cur in currents:
+                if (cur[:]) not in next: next.append(cur[:])
+                if (cur+[num]) not in next: next.append(cur+[num])
+            currents = next
+        return currents
+
+    def combination(self, n, k):
+        return self.combinationRecur(range(1,n+1), k, 0)
+
+    def combinationRecur(self, nums, k, start):
+        if k<=0: return [[]]
+        if k>=len(nums): return [nums[:]]
+        res = []
+        for i in range(start, len(nums)):
+            remains = self.combinationRecur(nums, k, i+1)
+            for remain in remains: res.append(remain)
+            remains = self.combinationRecur(nums, k-1, i+1)
+            for remain in remains: res.append([nums[i]]+remain)
+        return res
+
+if __name__ == "__main__":
+    s = Solution()
+    print s.permutation([1,2,3])
+    print s.permutation2([1,1,2])
+    print s.subsets([1,2,3])
+    print s.subsets2([1,1,2])
+    print s.combination(4,2)
